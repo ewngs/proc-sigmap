cd110d9d9d2362f213ff4fd4480d6f5838b0f6e9
Péter Kovács <kiwep@ewn.gs>
2016-06-09 15:54:42 +0200

    Proper test added

--- test/test.js
+++ test/test.js
@@ -1,14 +1,43 @@
 'use strict';
 
+var test = require('tape');
 var processEvents = require('..')({
-    SIGINT: 'beeboobaa'
+    SIGINT: 'shutdown',
+    reload: 'reload'
 });
 
-var timer = setInterval(function () {
-    console.log('tick');
-}, 1000);
+test('basic interface', function (t) {
+    t.plan(3);
+    t.ok(typeof processEvents === 'object' && processEvents.constructor && processEvents.constructor.name === 'SigMapper', 'got a SigMapper instance');
+    t.ok(typeof processEvents.on === 'function', 'on method visible');
+    t.ok(typeof processEvents.disconnect === 'function', 'disconnect method visible');
+});
 
-processEvents.on('beeboobaa', function () {
-    console.log('should shutdown');
-    clearInterval(timer);
+test('process event mapping', function (t) {
+    var sigintTimer, reloadTimer;
+
+    t.plan(2);
+
+    sigintTimer = setTimeout(function () {
+        t.fail('SIGINT event received');
+    }, 100);
+
+    processEvents.on('shutdown', function () {
+        clearTimeout(sigintTimer);
+        t.pass('SIGINT event received');
+    });
+
+    process.emit('SIGINT');
+
+    reloadTimer = setTimeout(function () {
+        t.fail('reload event received');
+    }, 100);
+
+    processEvents.on('reload', function () {
+        clearTimeout(reloadTimer);
+        t.pass('reload event received');
+    });
+
+    process.emit('reload');
+
 });
